6edc9ae58966c4180af67c0b5f5a114498908636
Pranay Garlapati <pgarlapati@zuora.com>
2023-10-18 13:22:26 +0530

    Initial commit

---
+++ src/main/java/com/dattebayo/dattebayo/DattebayoApplication.java
@@ -0,0 +1,13 @@
+package com.dattebayo.dattebayo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DattebayoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DattebayoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dattebayo/dattebayo/DattebayoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dattebayo.dattebayo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DattebayoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
